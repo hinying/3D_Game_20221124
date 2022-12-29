948e87d532141559bbf337e0f3d402a2be0dbc30
hinying <lisawu3032117@gmail.com>
2022-12-29 18:37:19 +0800

    Week13 完成圖示

--- 3D_Game_20221124/Assets/Script/DialogueSystem.cs
+++ 3D_Game_20221124/Assets/Script/DialogueSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 
 namespace HSIN
@@ -9,7 +10,7 @@ namespace HSIN
     /// </summary>
     public class DialogueSystem : MonoBehaviour
     {
-        #region MyRegion
+        #region 資料區域
         [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
         private float dialogueIntervalTime = 0.1f;
         [SerializeField, Header("開頭對話")]
@@ -19,9 +20,10 @@ namespace HSIN
         private CanvasGroup groupDialogue;
         private TextMeshProUGUI textName;
         private TextMeshProUGUI textContent;
-        private GameObject goTriangle; 
+        private GameObject goTriangle;
         #endregion
 
+        #region 事件區域
         private void Awake()
         {
             groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
@@ -29,7 +31,42 @@ namespace HSIN
             textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
             goTriangle = GameObject.Find("對話完成圖示");
             goTriangle.SetActive(false);
+
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect());
         }
+        #endregion
+        /// <summary>
+        /// 淡入淡出群組物件
+        /// </summary>
+        private IEnumerator FadeGroup()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                groupDialogue.alpha += 0.1f;
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+
+        /// <summary>
+        /// 打字效果
+        /// </summary>
+        private IEnumerator TypeEffect()
+        {
+            textName.text = dialogueOpening.dialogueName;
+            textContent.text = "";
+
+            string dialogue = dialogueOpening.dialogueContent[1];
+
+            for (int i = 0; i < dialogue.Length; i++)
+            {
+                textContent.text += dialogue[i];
+                yield return dialogueInterval;
+            }
+
+            goTriangle.SetActive(true);
+        }
+
     }
 }
 
