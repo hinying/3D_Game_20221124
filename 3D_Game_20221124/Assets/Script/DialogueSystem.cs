ccca284972a29a5809aea48734b3fd8c0fc5f512
hinying <lisawu3032117@gmail.com>
2022-12-29 17:01:10 +0800

    建立對話資料

---
+++ 3D_Game_20221124/Assets/Script/DialogueSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+namespace HSIN
+{
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public class DialogueSystem : MonoBehaviour
+    {
+        #region MyRegion
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData dialogueOpening;
+
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContent;
+        private GameObject goTriangle; 
+        #endregion
+
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話完成圖示");
+            goTriangle.SetActive(false);
+        }
+    }
+}
+
